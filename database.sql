6982d28325275d619b51f556294a73d2d782d9ad
Jumts <julianamattos201313@gmail.com>
2020-01-23 16:58:05 -0300

    edição de valores

--- database.sql
+++ database.sql
@@ -8,3 +8,8 @@ create table cliente(
 
 INSERT INTO cliente (nome_cliente, idade_cliente, email_cliente) VALUES ('Juliana', 23, 'ju@mail.com')
 INSERT INTO cliente (nome_cliente, idade_cliente, email_cliente) VALUES ('Maria', 28, 'maria@mail.com')
+
+UPDATE cliente SET nome_cliente = 'Helena' WHERE id_cliente = 2
+UPDATE cliente SET idade_cliente = 20
+UPDATE cliente SET email_cliente = 'mail@mail.com' WHERE idade_cliente > 18
+UPDATE cliente SET idade_cliente = 25 WHERE idade_cliente % 2 = 0
