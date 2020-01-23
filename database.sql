3f056bd46d69d0906e3bd7d1cd296380ad66a141
Jumts <julianamattos201313@gmail.com>
2020-01-23 16:46:57 -0300

    inserção de dados na tabela

--- database.sql
+++ database.sql
@@ -5,3 +5,6 @@ create table cliente(
     email_cliente varchar(150) not null,
     PRIMARY KEY (id_cliente)
 )
+
+INSERT INTO cliente (nome_cliente, idade_cliente, email_cliente) VALUES ('Juliana', 23, 'ju@mail.com')
+INSERT INTO cliente (nome_cliente, idade_cliente, email_cliente) VALUES ('Maria', 28, 'maria@mail.com')
