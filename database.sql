2caa87a754a82ecf5b7d88582da221313e6bc35c
Jumts <julianamattos201313@gmail.com>
2020-01-23 17:13:59 -0300

    Seleção de valores da tabela

--- database.sql
+++ database.sql
@@ -13,3 +13,15 @@ UPDATE cliente SET nome_cliente = 'Helena' WHERE id_cliente = 2
 UPDATE cliente SET idade_cliente = 20
 UPDATE cliente SET email_cliente = 'mail@mail.com' WHERE idade_cliente > 18
 UPDATE cliente SET idade_cliente = 25 WHERE idade_cliente % 2 = 0
+UPDATE cliente SET status = 'Ativo'
+
+ALTER TABLE cliente ADD status varchar(100)
+
+SELECT * FROM cliente
+SELECT c.nome_cliente FROM cliente c WHERE id_cliente 
+SELECT * FROM cliente WHERE id_cliente = 1 
+SELECT * FROM cliente c WHERE c.status = 'Ativo'
+SELECT * FROM cliente WHERE idade_cliente = 25 
+
+
+INSERT INTO cliente (nome_cliente, idade_cliente, email_cliente) VALUES ('Carlos', 34, 'carlos@mail.com', 'Inativo')
